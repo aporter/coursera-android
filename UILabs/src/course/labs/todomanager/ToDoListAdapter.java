b71a589ac59670bf867172d7e208c2299f9b50b9
lallo <claudio.cavalera@gmail.com>
2014-02-17 17:28:14 +0100

    UILabs homework done

--- UILabs/src/course/labs/todomanager/ToDoListAdapter.java
+++ UILabs/src/course/labs/todomanager/ToDoListAdapter.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -86,12 +87,12 @@ public class ToDoListAdapter extends BaseAdapter {
 
 
 		//TODO - Get the current ToDoItem
-		final ToDoItem toDoItem = null;
+		final ToDoItem toDoItem = (ToDoItem) getItem(position);
 
 		//TODO - Inflate the View for this ToDoItem
 		// from todo_item.xml.
-		RelativeLayout itemLayout = null;
-		
+		LayoutInflater mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		RelativeLayout itemLayout = (RelativeLayout) mLayoutInflater.inflate(R.layout.todo_item, null);
 		//TODO - Fill in specific ToDoItem data
 		// Remember that the data that goes in this View
 		// corresponds to the user interface elements defined 
@@ -99,12 +100,13 @@ public class ToDoListAdapter extends BaseAdapter {
 
 		//TODO - Display Title in TextView
 
-		final TextView titleView = null;
+		final TextView titleView = (TextView) itemLayout.findViewById(R.id.titleView);
+		titleView.setText(toDoItem.getTitle());
 		
-		// TODO - Set up Status CheckBox
+		//TODO - Set up Status CheckBox
 	
-		final CheckBox statusView = null;
-		
+		final CheckBox statusView = (CheckBox) itemLayout.findViewById(R.id.statusCheckBox);
+		statusView.setChecked(toDoItem.getStatus()==Status.DONE?true:false);
 		
 		statusView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
@@ -112,9 +114,18 @@ public class ToDoListAdapter extends BaseAdapter {
 					boolean isChecked) {
 				log("Entered onCheckedChanged()");
 				
-				// TODO - Set up and implement an OnCheckedChangeListener, which 
+				//TODO - Set up and implement an OnCheckedChangeListener, which 
 				// is called when the user toggles the status checkbox
-
+				if (isChecked) {
+					toDoItem.setStatus(Status.DONE);
+					statusView.setChecked(true);
+				}
+				
+				else {
+					toDoItem.setStatus(Status.NOTDONE);
+					statusView.setChecked(false);
+				}
+					
 
 			
 			}
@@ -122,14 +133,14 @@ public class ToDoListAdapter extends BaseAdapter {
 
 		//TODO - Display Priority in a TextView
 
-		final TextView priorityView = null;
-
+		final TextView priorityView = (TextView) itemLayout.findViewById(R.id.priorityView);
+		priorityView.setText(toDoItem.getPriority().toString());
 		
-		// TODO - Display Time and Date. 
+		//TODO - Display Time and Date. 
 		// Hint - use ToDoItem.FORMAT.format(toDoItem.getDate()) to get date and time String
-
-		final TextView dateView = null;
-				
+		
+		final TextView dateView = (TextView) itemLayout.findViewById(R.id.dateView);
+		dateView.setText(ToDoItem.FORMAT.format(toDoItem.getDate()));
 
 		// Return the View you just created
 		return itemLayout;
--- UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
+++ UILabs/src/course/labs/todomanager/ToDoManagerActivity.java
@@ -51,9 +51,10 @@ public class ToDoManagerActivity extends ListActivity {
 
 		//TODO - Inflate footerView for footer_view.xml file
 
-		TextView footerView = null;
+		TextView footerView = (TextView) getLayoutInflater().inflate(R.layout.footer_view, null);
 
 		//TODO - Add footerView to ListView
+		getListView().addFooterView(footerView);
 
 		footerView.setOnClickListener(new OnClickListener() {
 			@Override
@@ -62,12 +63,14 @@ public class ToDoManagerActivity extends ListActivity {
 				log("Entered footerView.OnClickListener.onClick()");
 
 				//TODO - Attach Listener to FooterView. Implement onClick().
+				Intent intent1 = new Intent (getBaseContext(), AddToDoActivity.class);
+				startActivityForResult(intent1, ADD_TODO_ITEM_REQUEST);
 
 			}
 		});
 
 		//TODO - Attach the adapter to this ListActivity's ListView
-
+		getListView().setAdapter(mAdapter);
 	}
 
 	@Override
@@ -79,6 +82,10 @@ public class ToDoManagerActivity extends ListActivity {
 		// If user submitted a new ToDoItem
 		// Create a new ToDoItem from the data Intent
 		// and then add it to the adapter
+		if ((requestCode == ADD_TODO_ITEM_REQUEST) && (resultCode == RESULT_OK)) {
+			ToDoItem newToDoItem = new ToDoItem(data);
+			mAdapter.add(newToDoItem);
+		}
 
 	}
 
