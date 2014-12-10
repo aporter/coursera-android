f63fa9235ea6cf8d5084635d6d3d617b283f6da9
aporter <aporter@cs.umd.edu>
2014-12-09 20:25:36 -0500

    replace tabs with spaces

--- Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/HelloAndroid.java
+++ Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/HelloAndroid.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class HelloAndroid extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
 
-		// Required call through to Activity.onCreate()
-		// Restore any saved instance state
-		super.onCreate(savedInstanceState);
+        // Required call through to Activity.onCreate()
+        // Restore any saved instance state
+        super.onCreate(savedInstanceState);
 
-		// Set up the application's user interface (content view)
-		setContentView(R.layout.helloandroid);
-	}
+        // Set up the application's user interface (content view)
+        setContentView(R.layout.helloandroid);
+    }
 }
--- Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/LoginScreen.java
+++ Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/LoginScreen.java
@@ -12,38 +12,38 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginScreen extends Activity {
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.loginscreen);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginscreen);
 
-		final EditText uname = (EditText) findViewById(R.id.username_edittext);
-		final EditText passwd = (EditText) findViewById(R.id.password_edittext);
-		
-		final Button loginButton = (Button) findViewById(R.id.login_button);
-		loginButton.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				
-				if (checkPassword(uname.getText(), passwd.getText())) {
-					
-					// Create an explicit Intent for starting the HelloAndroid Activity 
-					Intent helloAndroidIntent = new Intent(LoginScreen.this,
-							HelloAndroid.class);
-					
-					// Use the Intent to start the HelloAndroid Activity 
-					startActivity(helloAndroidIntent);
+        final EditText uname = (EditText) findViewById(R.id.username_edittext);
+        final EditText passwd = (EditText) findViewById(R.id.password_edittext);
+        
+        final Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                
+                if (checkPassword(uname.getText(), passwd.getText())) {
+                    
+                    // Create an explicit Intent for starting the HelloAndroid Activity 
+                    Intent helloAndroidIntent = new Intent(LoginScreen.this,
+                            HelloAndroid.class);
+                    
+                    // Use the Intent to start the HelloAndroid Activity 
+                    startActivity(helloAndroidIntent);
 
-				} else {
-					uname.setText("");
-					passwd.setText("");
-				}
-			}
-		});
-	}
+                } else {
+                    uname.setText("");
+                    passwd.setText("");
+                }
+            }
+        });
+    }
 
-	private boolean checkPassword(Editable uname, Editable passwd) {
-		// Just pretending to extract text and check password
-		return new Random().nextBoolean();
-	}
+    private boolean checkPassword(Editable uname, Editable passwd) {
+        // Just pretending to extract text and check password
+        return new Random().nextBoolean();
+    }
 }
