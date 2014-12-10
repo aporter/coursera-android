ba37b4ed594eff674dad1c51a1306aa49e98646e
aporter <aporter@cs.umd.edu>
2014-12-10 08:21:34 -0500

    remove tabs

--- Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/LoginScreen.java
+++ Examples/HelloAndroidWithLogin/src/course/examples/helloWorldWithLogin/LoginScreen.java
@@ -19,19 +19,20 @@ public class LoginScreen extends Activity {
 
         final EditText uname = (EditText) findViewById(R.id.username_edittext);
         final EditText passwd = (EditText) findViewById(R.id.password_edittext);
-        
+
         final Button loginButton = (Button) findViewById(R.id.login_button);
         loginButton.setOnClickListener(new OnClickListener() {
-            
+
             public void onClick(View v) {
-                
+
                 if (checkPassword(uname.getText(), passwd.getText())) {
-                    
-                    // Create an explicit Intent for starting the HelloAndroid Activity 
+
+                    // Create an explicit Intent for starting the HelloAndroid
+                    // Activity
                     Intent helloAndroidIntent = new Intent(LoginScreen.this,
                             HelloAndroid.class);
-                    
-                    // Use the Intent to start the HelloAndroid Activity 
+
+                    // Use the Intent to start the HelloAndroid Activity
                     startActivity(helloAndroidIntent);
 
                 } else {
