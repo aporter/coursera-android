531b1db17c9502cd219c62275477526d1cf720a9
lutaoact <lutaoact@hotmail.com>
2015-05-08 13:03:22 +0800

    getLayoutInflater().inflate(R.layout.custom_toast, mainLinearLayout, false)

--- Examples/NotificationToastWithCustomView/src/course/examples/notification/toastwithcustomview/NotificationToastActivity.java
+++ Examples/NotificationToastWithCustomView/src/course/examples/notification/toastwithcustomview/NotificationToastActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,6 +14,8 @@ public class NotificationToastActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final ViewGroup mainLinearLayout = (ViewGroup)findViewById(R.id.main_linear_layout);
         
 		Button button = (Button) findViewById(R.id.toast_button);
 		button.setOnClickListener(new OnClickListener() {
@@ -25,7 +28,7 @@ public class NotificationToastActivity extends Activity {
 		        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
 		        toast.setDuration(Toast.LENGTH_LONG);
 		        
-		        toast.setView(getLayoutInflater().inflate(R.layout.custom_toast,null));
+		        toast.setView(getLayoutInflater().inflate(R.layout.custom_toast, mainLinearLayout, false));
  
 		        toast.show();
 			}
