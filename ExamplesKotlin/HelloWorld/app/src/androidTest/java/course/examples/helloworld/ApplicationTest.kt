e97b354e1155a95cd57b595f98d164cc2af206c1
Adam A. Porter <aporter@umd.edu>
2019-07-26 16:21:30 -0400

    Convert to Kotlin

---
+++ ExamplesKotlin/HelloWorld/app/src/androidTest/java/course/examples/helloworld/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package course.examples.helloworld
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
