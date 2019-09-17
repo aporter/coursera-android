4ce46ddedf169492cb4668d121eaf16d6e5b52ac
Adam A. Porter <aporter@umd.edu>
2019-09-17 13:35:43 -0400

    Convert to Kotlin

---
+++ ExamplesKotlin/NotificationStatusBarWithCustomView/app/src/main/java/course/examples/notification/statusbarwithcustomview/NotificationSubActivity.kt
@@ -0,0 +1,12 @@
+package course.examples.notification.statusbarwithcustomview
+
+import android.app.Activity
+import android.os.Bundle
+
+class NotificationSubActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sub_activity)
+    }
+}
