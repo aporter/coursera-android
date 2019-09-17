43819d5955fcc82ae49381beab998e69a3eff8c3
Adam A. Porter <aporter@umd.edu>
2019-09-17 13:39:25 -0400

    Convert to Kotlin

---
+++ ExamplesKotlin/UIConstraintLayout/app/src/main/java/course/examples/ui/linearlayout/LinearLayoutActivity.kt
@@ -0,0 +1,12 @@
+package course.examples.ui.linearlayout
+
+import android.app.Activity
+import android.os.Bundle
+
+class LinearLayoutActivity : Activity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
